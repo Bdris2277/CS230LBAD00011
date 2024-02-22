598e81fa18e1bfd53df3e53f46c1f8ee97581780
Bdris2277 <bdriscoll2003@gmail.com>
2024-02-22 12:53:44 -0500

    commit3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1> CS 230L </h1>
       <h2> Section - 002</h2>
       <p> WVU ID: 800363370</p>
-      <p> Hi I am Bryce Driscoll!</p>
+      <p> Hi I am Bryce Driscollx</p>
     </div>
   );
 }
