9855fc8a80e0afd63a0f668d63d01f9770718467
Bdris2277 <bdriscoll2003@gmail.com>
2024-03-03 21:15:33 -0500

    //finished this

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Navbar.js'; 
+import Card from './Card.js'; 
 
 function App() {
   return (
     <div>
+      <NavBar />
       <h1> CS 230L </h1>
       <h2> Section - 002</h2>
       <p> WVU ID: 800363370</p>
-      <p> Hi I am Bryce Driscollx</p>
+      <p> Hi I am Bryce Driscoll</p>
+      <Card />
     </div>
   );
 }
